55ac72949f845763ddc87f16ef69972b0bccefb4
wick3dr0se <wick3dr0se@protonmail.com>
2022-09-10 12:28:22 -0500

    make pull handle various arguments

--- endpoints/pull.sh
+++ endpoints/pull.sh
@@ -5,16 +5,27 @@
 ghc_pull() { # accept user, repo & branch
   user="${1:-$owner}"
   repo="${2:-$relDir}"
-  [[ $3 ]] && branch="$3" || branch="${1##*/}"
+  
+  if [[ $3 ]] ; then
+    branch="$3"
+  else 
+    branch="${1##*/}"
+  fi
 
-  [[ $1 || $1 =~ / ]] && repo="${1}"
-  [[ $2 ]] || user=''
+  if [[ ! $2 ]] ; then
+    user="$owner"
+    repo="$1"
+  fi
 
   if [[ $1 =~ / ]] ; then
     user="${1%%/*}"
     repo="${1#*/}"
     repo="${repo/\/$branch}"
   fi
+
+  [[ $branch =~ $repo|$user ]] && branch=''
+
+  echo $user $repo $branch
 
   flags='-L'
   ghc GET "repos/${user}/${repo}/tarball/${branch}" | tar xz
