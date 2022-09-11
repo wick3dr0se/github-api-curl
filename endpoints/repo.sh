e41d3bf727778078e20431cb111a96bc9071c621
wick3dr0se <wick3dr0se@protonmail.com>
2022-09-10 21:08:32 -0500

    add repo create & delete methods

--- endpoints/repo.sh
+++ endpoints/repo.sh
@@ -3,18 +3,38 @@
 
 # fetches specifiable repository information
 ghc_repo() { # accepts user & repo
-  user="${1:-$owner}"
-  repo="${2:-$relDir}"
-
-  if [[ ! $2 ]] ; then
+  if [[ $2 ]] ; then
+    user="${1:-$owner}"
+    repo="${2:-$relDir}"
+  else
     if [[ $1 =~ / ]] ; then
       user="${1%%/*}"
       repo="${1##*/}"
     else
       user="$owner"
-      repo="$1"
+      repo="${1:-$relDir}"
     fi
   fi
 
   ghc GET "repos/${user}/${repo}"
+}
+
+ghc_repo_create() { # accepts repo & description
+  repo="${1:-$relDir}"
+  description="${@/$repo}"
+
+  echo $user $repo ${description}
+
+  ghc POST "user/repos" "{
+    \"name\": \"${repo}\",
+    \"description\": \"${description}\",
+    \"auto_init\": true,
+    \"private\": false
+  }"
+}
+
+ghc_repo_delete() { # accepts repo 
+  repo="${1:-$relDir}"
+
+  ghc DELETE "repos/${owner}/${repo}"
 }
